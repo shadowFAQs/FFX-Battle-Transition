a7891e2ae995dd64f9db5327cec2abd91d00f412
Andrew L <alawler6@gmail.com>
2024-01-21 13:26:54 -0500

    Scale individual polygons to create gaps between them

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import os
 
 import numpy as np
 import pygame as pg
+
 from pygame import gfxdraw
 from scipy.spatial import Voronoi
+from shapely.geometry import Polygon
+from shapely.affinity import scale
 
 
 class Transition():
@@ -11,7 +14,7 @@ class Transition():
         self.clock = pg.time.Clock()
         self.frame_counter = 0
         self.image = None
-        self.num_vertices = 20
+        self.num_polygons = 30
         self.screencap = None
         self.polygons = []
         self.state = ''
@@ -24,21 +27,15 @@ class Transition():
             self.frame_counter = 1
 
     def create_shards(self):
-        # Generate random 2D points
-        points = np.random.rand(self.num_vertices, 2) * 512
-        edge_points = np.array([[-128, -128], [-128, 768], [768, -128], [768, 768]])
-        points = np.append(points, edge_points, axis=0)
+        random_seeds = np.random.rand(self.num_polygons, 2) * 512
+        vor = Voronoi(random_seeds)
+        regions, vertices = voronoi_finite_polygons_2d(vor)
 
-        # Compute Voronoi tesselation
-        vor = Voronoi(points)
-
-        # Print the coordinates of each vertex
-        for i, region in enumerate(vor.regions):
-            if len(region) > 0:
-                polygon = []
-                for ind in region:
-                    polygon.append(vor.vertices[ind])
-                self.polygons.append(polygon)
+        self.polygons = []
+        for reg in regions:
+            poly = Polygon(vertices[reg])
+            scaled_poly = scale(poly, xfact=0.98, yfact=0.98, origin='centroid')
+            self.polygons.append(scaled_poly)
 
     def draw(self):
         self.image.fill(pg.Color(0, 0, 0))
@@ -46,9 +43,10 @@ class Transition():
         if not self.polygons:
             self.image.blit(self.screencap, (0, 0))
 
-        for polygon in self.polygons:
-            gfxdraw.filled_polygon(self.image, polygon, pg.Color(255, 0, 0))
-            gfxdraw.polygon(self.image, polygon, pg.Color(255, 255, 255))
+        for poly in self.polygons:
+            coords = list(poly.exterior.coords)
+            gfxdraw.filled_polygon(self.image, coords, pg.Color(255, 0, 0))
+            gfxdraw.polygon(self.image, coords, pg.Color(255, 255, 255))
 
     def end(self):
         print('end')
@@ -85,6 +83,88 @@ class Transition():
 
         if self.frame_counter:
             self.frame_counter += 1
+
+
+def voronoi_finite_polygons_2d(vor, radius=None):
+    """
+    Reconstruct infinite voronoi regions in a 2D diagram to finite
+    regions.
+    Parameters
+    ----------
+    vor : Voronoi
+        Input diagram
+    radius : float, optional
+        Distance to 'points at infinity'.
+    Returns
+    -------
+    regions : list of tuples
+        Indices of vertices in each revised Voronoi regions.
+    vertices : list of tuples
+        Coordinates for revised Voronoi vertices. Same as coordinates
+        of input vertices, with 'points at infinity' appended to the
+        end.
+    """
+
+    if vor.points.shape[1] != 2:
+        raise ValueError("Requires 2D input")
+
+    new_regions = []
+    new_vertices = vor.vertices.tolist()
+
+    center = vor.points.mean(axis=0)
+    if radius is None:
+        radius = vor.points.ptp().max() * 2
+        print(radius)
+
+    # Construct a map containing all ridges for a given point
+    all_ridges = {}
+    for (p1, p2), (v1, v2) in zip(vor.ridge_points, vor.ridge_vertices):
+        all_ridges.setdefault(p1, []).append((p2, v1, v2))
+        all_ridges.setdefault(p2, []).append((p1, v1, v2))
+
+    # Reconstruct infinite regions
+    for p1, region in enumerate(vor.point_region):
+        vertices = vor.regions[region]
+
+        if all(v >= 0 for v in vertices):
+            # finite region
+            new_regions.append(vertices)
+            continue
+
+        # reconstruct a non-finite region
+        ridges = all_ridges[p1]
+        new_region = [v for v in vertices if v >= 0]
+
+        for p2, v1, v2 in ridges:
+            if v2 < 0:
+                v1, v2 = v2, v1
+            if v1 >= 0:
+                # finite ridge: already in the region
+                continue
+
+            # Compute the missing endpoint of an infinite ridge
+
+            t = vor.points[p2] - vor.points[p1] # tangent
+            t /= np.linalg.norm(t)
+            n = np.array([-t[1], t[0]])  # normal
+
+            midpoint = vor.points[[p1, p2]].mean(axis=0)
+            direction = np.sign(np.dot(midpoint - center, n)) * n
+            far_point = vor.vertices[v2] + direction * radius
+
+            new_region.append(len(new_vertices))
+            new_vertices.append(far_point.tolist())
+
+        # sort region counterclockwise
+        vs = np.asarray([new_vertices[v] for v in new_region])
+        c = vs.mean(axis=0)
+        angles = np.arctan2(vs[:,1] - c[1], vs[:,0] - c[0])
+        new_region = np.array(new_region)[np.argsort(angles)]
+
+        # finish
+        new_regions.append(new_region.tolist())
+
+    return new_regions, np.asarray(new_vertices)
 
 
 def main():
