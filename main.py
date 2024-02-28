72e9c1d29e2d27f77fa9f6dc29fa9cb20e204f0b
Andrew L <alawler6@gmail.com>
2024-02-28 05:56:29 -0500

    Remove bounding box; tweak animation values

--- main.py
+++ main.py
@@ -22,24 +22,25 @@ YELLOW      = pg.Color('#f5b631')
 
 
 class Shard():
-    def __init__(self, image: pg.Surface, poly: Polygon, offset: tuple[int]):
+    def __init__(self, image: pg.Surface, poly: Polygon):
         self.poly = poly
-        self.offset = offset
 
         x_min, y_min, x_max, y_max = self.poly.bounds
         poly_width = x_max - x_min
         poly_height = y_max - y_min
 
-        self.cropped_rect = pg.Rect(x_min - self.offset[0], y_min - self.offset[1], poly_width, poly_height)
-        self.cropped_image = image.subsurface(self.cropped_rect)
+        self.display = True
         self.friction = 0.92
         self.in_motion = False
         self.masked_poly = None  # pg.Surface
         self.motion_frame = 0
-        self.rotated_rect = self.cropped_rect.copy()
         self.rotation_angle = 0
-        self.topleft = (x_min - self.offset[0], y_min - self.offset[1])
+        self.topleft = (x_min, y_min)
         self.tween_coords = []
+
+        self.cropped_rect = pg.Rect(x_min, y_min, poly_width, poly_height)
+        self.cropped_image = image.subsurface(self.cropped_rect)
+        self.rotated_rect = self.cropped_rect.copy()
 
         self.create_masked_poly()
         self.set_rotation(-1, 1)  # Create a little initial motion in the pattern of cracks
@@ -53,16 +54,15 @@ class Shard():
         return self.poly.centroid.x, self.poly.centroid.y
 
     def create_masked_poly(self):
-        poly_points = [tuple([p[0] - self.topleft[0] - self.offset[0],
-                             p[1] - self.topleft[1] - self.offset[1]]) for p in self.poly.exterior.coords]
+        poly_points = [tuple([p[0] - self.topleft[0], p[1] - self.topleft[1]]) for p in self.poly.exterior.coords]
 
         surface = self.cropped_image.copy()
-        surface.fill(pg.Color('#000000'))
-        pg.draw.polygon(surface, pg.Color('#ffffff'), poly_points)
+        surface.fill(pg.Color(0, 0, 0))
+        pg.draw.polygon(surface, pg.Color(255, 255, 255), poly_points)
         surface.blit(self.cropped_image, (0, 0), special_flags=pg.BLEND_RGBA_MIN)
 
         pxarray = pg.PixelArray(surface)
-        pxarray.replace(pg.Color('#000000'), TRANSPARENT)
+        pxarray.replace(pg.Color(0, 0, 0), TRANSPARENT)
 
         self.masked_poly = surface
         self.masked_poly = pxarray.make_surface()
@@ -107,52 +107,38 @@ class Shard():
             self.translate()
             self.motion_frame = min(self.motion_frame + 1, len(self.tween_coords) - 1)
 
+            if self.motion_frame == len(self.tween_coords) - 1:
+                self.display = False
+
         self.set_topleft()
 
 
-def create_shards(bounding_box_offset: pg.Vector2, bounding_box_size: pg.Vector2, image: pg.Surface) -> list[Shard]:
-    vertices = create_vertices(bounding_box_offset, bounding_box_size)
-    return create_voronoi_shards(vertices, bounding_box_offset, bounding_box_size, image)
+def create_shards(screen_dims: tuple[int], image: pg.Surface) -> list[Shard]:
+    vertices = create_vertices(screen_dims)
+    return create_voronoi_shards(vertices, screen_dims, image)
 
 
-def create_vertices(bounding_box_offset: pg.Vector2, bounding_box_size: pg.Vector2,
-                    num_vertices:int = 100) -> list[pg.Vector2]:
-    bounding_box_offset = pg.Vector2(bounding_box_offset)
-    bounding_box_size = pg.Vector2(bounding_box_size)
-
+def create_vertices(screen_dims: tuple[int], num_vertices: int = 100) -> list[pg.Vector2]:
     random_vertices = [pg.Vector2(p) for p in list(zip(
-        np.random.uniform(bounding_box_offset.x, bounding_box_offset.x + bounding_box_size.x, num_vertices),
-        np.random.uniform(bounding_box_offset.y, bounding_box_offset.y + bounding_box_size.y, num_vertices)))]
+        np.random.uniform(0, screen_dims[0], num_vertices),
+        np.random.uniform(0, screen_dims[1], num_vertices)))]
 
-    # For each vert, reflect it across the vertical and horizontal axes it's closest to.
-    # Adding these reflected verts to our initial set ensures the diagram includes the edges of our bounding box.
+    # For each vert, reflect it across all screen edges.
+    # Adding these reflected verts to the initial set ensures the diagram includes lines along the screen edges.
     reflected = []
     for pt in random_vertices:
-        boundary_line_right = bounding_box_offset.x + bounding_box_size.x
-        delta = boundary_line_right - pt.x
-        if delta < bounding_box_offset.x:
-            reflected.append(pg.Vector2(boundary_line_right + delta, pt.y))
+        delta = screen_dims[0] - pt.x
+        reflected.append(pg.Vector2(pt.x * -1, pt.y))               # Reflect across left edge
+        reflected.append(pg.Vector2(screen_dims[0] + delta, pt.y))  # Reflect across right edge
 
-        boundary_line_left = bounding_box_offset.x
-        delta = boundary_line_left - pt.x
-        if delta < bounding_box_offset.x:
-            reflected.append(pg.Vector2(boundary_line_left + delta, pt.y))
-
-        boundary_line_top = bounding_box_offset.y
-        delta = pt.y - boundary_line_top
-        if delta < bounding_box_offset.y:
-            reflected.append(pg.Vector2(pt.x, boundary_line_top - delta))
-
-        boundary_line_bottom = bounding_box_offset.y + bounding_box_size.y
-        delta = boundary_line_bottom - pt.y
-        if delta < bounding_box_offset.y:
-            reflected.append(pg.Vector2(pt.x, boundary_line_bottom + delta))
+        delta = screen_dims[1] - pt.y
+        reflected.append(pg.Vector2(pt.x, screen_dims[1] + delta))  # Reflect across bottom edge
+        reflected.append(pg.Vector2(pt.x, pt.y * -1))               # Reflect across top edge
 
     return random_vertices + reflected
 
 
-def create_voronoi_shards(vertices: list[pg.Vector2], bounding_box_offset: pg.Vector2,
-                          bounding_box_size: pg.Vector2, image: pg.Surface) -> list[Shard]:
+def create_voronoi_shards(vertices: list[pg.Vector2], screen_dims: tuple[int], image: pg.Surface) -> list[Shard]:
     vor = Voronoi(np.array([[v.x, v.y] for v in vertices]))
 
     polygons = []
@@ -162,25 +148,26 @@ def create_voronoi_shards(vertices: list[pg.Vector2], bounding_box_offset: pg.Ve
     # Drop polygons which have any points outside the bounding box
     filtered_polys = []
     for poly in polygons:
-        drop = False
+        valid = True
 
-        if not len(poly.exterior.coords):
-            drop = True
+        if len(poly.exterior.coords):
+            for pt in poly.exterior.coords:
+                if pt[0] < -1 or pt[0] > screen_dims[0] + 1:
+                    valid = False
+                    break
+                if pt[1] < -1 or pt[1] > screen_dims[1] + 1:
+                    valid = False
+                    break
 
-        for pt in poly.exterior.coords:
-            if pt[0] < bounding_box_offset.x - 1 or pt[0] > bounding_box_offset.x + bounding_box_size.x + 1:
-                drop = True
-            if pt[1] < bounding_box_offset.y - 1 or pt[1] > bounding_box_offset.y + bounding_box_size.y + 1:
-                drop = True
-        if not drop:
-            filtered_polys.append(poly)
+            if valid:
+                filtered_polys.append(poly)
 
     polygons = None
 
     # Scale down each poly & create Shard objects
     shards = []
     for poly in filtered_polys:
-        shards.append(Shard(image, scale(poly, 0.9, 0.9), bounding_box_offset))
+        shards.append(Shard(image, scale(poly, 0.9, 0.9)))
 
     return shards
 
@@ -199,18 +186,17 @@ def main():
 
     pg.init()
     pg.display.set_caption('FFX Shatter Transition')
-    screen_dims = (768, 768)
-    bounding_box_offset = pg.Vector2(128, 128)
-    bounding_box_size = pg.Vector2(512, 512)
+    screen_dims = (512, 512)
     screen = pg.display.set_mode(screen_dims)
     clock = pg.time.Clock()
+    target_framerate = 30
 
     image = pg.image.load('zanarkand.png')
-    shards = create_shards(bounding_box_offset, bounding_box_size, image)
+    shards = create_shards(screen_dims, image)
     glare_alpha_max = 130
     glare_alpha = glare_alpha_max
     glare_counter = 1.0
-    sweep_x = bounding_box_offset[0]
+    sweep_x = 16
     motion_surface = pg.Surface(screen_dims, pg.SRCALPHA)
 
     cooldown_timer = 0
@@ -220,17 +206,17 @@ def main():
     running = True
 
     while running:
-        clock.tick(30)
+        clock.tick(target_framerate)
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
             elif event.type == pg.MOUSEBUTTONDOWN:
                 if reset_ready:
-                    shards = create_shards(bounding_box_offset, bounding_box_size, image)
+                    shards = create_shards(screen_dims, image)
                     glare_alpha = glare_alpha_max
                     glare_counter = 1.0
-                    sweep_x = bounding_box_offset[0]
+                    sweep_x = 16
 
                     cooldown_timer = 0
                     paused = True
@@ -245,14 +231,17 @@ def main():
             screen.fill(DARKEST)
 
         if paused:
-            screen.blit(image, bounding_box_offset)
+            screen.blit(image, (0, 0))
         else:
             if glare_alpha:
                 glare_surface = pg.Surface(screen_dims, pg.SRCALPHA)
 
             for shard in shards:
                 shard.update()
-                screen.blit(shard.rotated_image, shard.topleft)
+
+                if shard.display:
+                    screen.blit(shard.rotated_image, shard.topleft)
+
                 if glare_alpha:
                     pg.draw.polygon(glare_surface, pg.Color(255, 255, 255, glare_alpha),
                                     shard.poly.exterior.coords)
@@ -270,7 +259,7 @@ def main():
 
                 if motion_blur:
                     motion_surface.blit(screen, (0, 0))
-                    alpha = max(int(-0.3 * sweep_x + 310), 64)
+                    alpha = max(int(-0.15 * sweep_x + 170), 50)
                     motion_surface.fill(pg.Color(18, 18, 18, alpha))
 
                 motion_blur = sweep_x > 330  # Delay before starting blur
